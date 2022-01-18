ccff637a77a75fcdcfd6c081575a7d49356c20d7
ivo.junior <BRHBS0801@ambev.com.br>
2022-01-18 16:41:38 -0300

    commit initial

---
+++ aula-stack-queue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using aula.pilha;
+using aula.fila;
+using aula.lista;
+
+namespace aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /* int[] initialStack = new int[0];
+
+            PilhaRoots.Menu(initialStack); */
+
+            //Pilha.Menu(new Stack<int>());
+            //Fila.Menu(new Queue<int>());
+            Menu.main();
+        }
+    }
+}
---
+++ aula-stack-queue/lista/ListaLigada.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+
+namespace aula.lista
+{
+    public class ListaLigada
+    {
+        public Vagao Trem { get; set; }
+
+        public void InserirVagao(Vagao novoVagao) {
+            if(Trem == null) Trem = novoVagao;
+            else {
+                var vagaoAnterior = getUltimoVagao();
+                vagaoAnterior.VagaoAnterior = novoVagao;
+            }
+        }
+
+        public Vagao getUltimoVagao() {
+            var tempVagao = Trem;
+            while(tempVagao.VagaoAnterior != null) {
+                tempVagao = tempVagao.VagaoAnterior;
+            }
+            return tempVagao;
+        }
+
+        public Vagao? BuscarPorId(int id) {
+            var tempVagao = Trem;
+            while(tempVagao != null && tempVagao.Id != id) {
+                tempVagao = tempVagao.VagaoAnterior;
+            }
+            return tempVagao;
+        }
+
+        public List<Vagao> BuscarPorCarga(string carga) {
+            var tempVagao = Trem;
+            List<Vagao> listVagao = new List<Vagao>();
+            do{
+                if(tempVagao.Carga == carga)
+                    listVagao.Add(tempVagao);
+                tempVagao = tempVagao.VagaoAnterior;
+            } while(tempVagao != null);
+            return listVagao;
+        }
+
+        // public Vagao BuscarPorPeso(int peso) {
+        //     var tempVagao = Trem;
+        //     List<Vagao> listVagao = new List<Vagao>();
+        //     while(tempVagao.VagaoAnterior != null) {
+        //         if(tempVagao.Carga == carga)
+        //             listVagao.Add(tempVagao);
+        //         tempVagao = tempVagao.VagaoAnterior;
+        //     }
+        //     return listVagao;
+        // }
+
+        public List<Vagao> getVagoes() {
+            var _trem = new List<Vagao>();
+            var tempVagao = Trem;
+            while(tempVagao != null) {
+                _trem.Add(tempVagao);
+                tempVagao = tempVagao.VagaoAnterior;
+            };
+            return _trem;
+        }
+    }
+}
